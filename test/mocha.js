67e50f6bb2631901db7f5eb99a61a1d703abed24
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-04-21 00:52:57 +0530

    updating

--- test/mocha.js
+++ test/mocha.js
@@ -2,6 +2,6 @@ var nconf = require('nconf');
 
 nconf.overrides({
     'services': [
-        {"name": "service-configs", "version": "master", "domain": "accounts", "prefix": "/apis/v/configs"}
+        {"path": __dirname + '/..', "domain": "accounts", "prefix": "/apis/v/configs"}
     ]
 });
